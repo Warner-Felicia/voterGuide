f636d139095410d283f200fea581dc954c7d43cd
Warner-Felicia <foodleswarner@gmail.com>
2023-06-30 13:05:59 -0400

    getting candidates by county, sorting on frontend

---
+++ src/app/admin/candidates/candidate-print/candidate-print.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Document, Packer, Paragraph, HeadingLevel, TextRun, UnderlineType } from 'docx';
+import { saveAs } from 'file-saver'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CandidateService {
+
+  constructor() {
+
+  }
+
+
+
+
+
+}
